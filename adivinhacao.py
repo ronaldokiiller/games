6605d20e3b7385c0e13078c01ab838bc490c7346
Ronaldo Kiiller <ronaldo.kiiller@escola.pr.gov.br>
2023-06-04 17:44:20 -0300

    adicionado break e continue e testado se o número está entre 1 e 100

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,15 +5,19 @@ print("*********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
+
 
 # os parentesese são redundantes, ou seja, desnecessários
-while(rodada <= total_de_tentativas):
-    print("tentativa", rodada, "de", total_de_tentativas)
-    rodada = rodada + 1
+for rodada in range(1, total_de_tentativas + 1):
+    print("tentativa {} de {}".format(rodada, total_de_tentativas))
+
     chute_str = input("Digite o seu número: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -21,6 +25,7 @@ while(rodada <= total_de_tentativas):
 
     if(acertou):
         print("Parabéns! Você acertou!")
+        break
     else:
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
