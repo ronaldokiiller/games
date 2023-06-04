2aa168c836e2a6e9ea1391fc1e5bbb2b7e684c99
Ronaldo Kiiller <ronaldo.kiiller@escola.pr.gov.br>
2023-06-04 19:27:53 -0300

    usado valores randomicos para o número secreto

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,11 +1,14 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 
+print(numero_secreto)
 
 # os parentesese são redundantes, ou seja, desnecessários
 for rodada in range(1, total_de_tentativas + 1):
