2a54d700770fdd0e7ab88641276c4226912de2f9
Ronaldo Kiiller <ronaldo.kiiller@escola.pr.gov.br>
2023-06-05 15:57:14 -0300

    finalizado jogo da adivinhacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,9 +6,20 @@ print("*********************************")
 
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
 
-print(numero_secreto)
+print("Qual nível de dificuldade?", numero_secreto)
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 # os parentesese são redundantes, ou seja, desnecessários
 for rodada in range(1, total_de_tentativas + 1):
@@ -27,12 +38,18 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Parabéns! Você acertou!")
+        print("Parabéns! Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+        if (total_de_tentativas == rodada):
+            print("O numero secretor era {} e você fez {} pontos".format(numero_secreto,pontos))
+
+
 
 print("Fim do jogo")
